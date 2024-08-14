f32edc95dca98dcc1fde198466394786e723f5e8
SirojPY <srjjumanazarov@gmail.com>
2024-08-14 23:14:16 +0500

    push dars12.homework

---
+++ dars12.homework.py
@@ -0,0 +1,11 @@
+# topshiriq 2
+sozlar=["Hasan","Husan","Ali","Vali"]
+uzunlik=list(map(lambda x:len(x),sozlar))
+print(uzunlik)
+
+# topshiriq 3
+sonlar=[-1,1,2,3,-3,-10,45,6,-9]
+print(list(filter(lambda x:x>=0,sonlar)))
+
+
+
